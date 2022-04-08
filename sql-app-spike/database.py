820adae1dd6686468dcb9f8bbbfa39abe052c943
younesmahfoudi <younesmfd@gmail.com>
2022-04-09 01:02:56 +0200

    crud fastapi with mysql sqlAlchemy pymysql

---
+++ sql-app-spike/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root1234@localhost:6033/app_db"
+# SQLALCHEMY_DATABASE_URL = "mysql://test:test@localhost:6033/app_db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
