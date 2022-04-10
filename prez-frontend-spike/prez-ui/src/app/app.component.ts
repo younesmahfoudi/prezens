0395b86a585ef63d8e69228a04225d22e1e8f0f8
younesmahfoudi <younesmfd@gmail.com>
2022-04-10 17:22:47 +0200

    generate a pdf with jsPDF

---
+++ prez-frontend-spike/prez-ui/src/app/app.component.ts
@@ -0,0 +1,65 @@
+import { Component } from '@angular/core';
+
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'prez-ui';
+
+  public USERS = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "email": "sincere@april.biz",
+      "phone": "1-770-736-8031 x56442"
+    },
+    {
+      "id": 2,
+      "name": "Ervin Howell",
+      "email": "shanna@melissa.tv",
+      "phone": "010-692-6593 x09125"
+    },
+    {
+      "id": 3,
+      "name": "Clementine Bauch",
+      "email": "nathan@yesenia.net",
+      "phone": "1-463-123-4447",
+    },
+    {
+      "id": 4,
+      "name": "Patricia Lebsack",
+      "email": "julianne@kory.org",
+      "phone": "493-170-9623 x156"
+    },
+    {
+      "id": 5,
+      "name": "Chelsey Dietrich",
+      "email": "lucio@annie.ca",
+      "phone": "(254)954-1289"
+    },
+    {
+      "id": 6,
+      "name": "Mrs. Dennis",
+      "email": "karley@jasper.info",
+      "phone": "1-477-935-8478 x6430"
+    }
+  ];
+
+  public openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+      let fileWidth = 208;
+      let fileHeight = (canvas.height * fileWidth) / canvas.width;
+      const FILEURI = canvas.toDataURL('image/png');
+      let PDF = new jsPDF('p', 'mm', 'a4');
+      let position = 0;
+      PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+      PDF.save('angular-demo.pdf');
+    });
+  }
+}
