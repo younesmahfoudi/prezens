7402e9ec6c8e58e9788f898fcd9d44a8c5a5121d
younesmahfoudi <younesmfd@gmail.com>
2022-04-29 00:00:11 +0200

    nouvelles routes

--- prez-frontend-spike/prez-ui/src/modules/prof-view/core/domain/professor/porfessor.model.ts
+++
@@ -1,9 +0,0 @@
-import {Lesson} from "../lesson/lesson.model";
-
-export interface Professor {
-  email: string;
-  first_name: string;
-  last_name: string;
-  uid: number;
-  lessons: Lesson[];
-}
