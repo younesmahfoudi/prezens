a4a94c168202f15347caa037c092a0a8b4ca96c1
TRUFFIER Thibaut <truffierth@eisti.eu>
2022-05-06 14:41:41 +0200

    professor screen

--- prez-frontend/prez-ui/src/modules/core/domain/professor/professor.service.spec.ts
+++ prez-frontend/prez-ui/src/modules/core/domain/professor/professor.service.spec.ts
@@ -1,6 +1,6 @@
-import {TestBed} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
-import {ProfessorService} from './professor.service';
+import { ProfessorService } from './professor.service';
 
 describe('ProfessorService', () => {
   let service: ProfessorService;
