82f7875964b8c15da24c08d6d1c0924bb1733984
TRUFFIER Thibaut <truffierth@eisti.eu>
2022-05-06 14:41:41 +0200

    professor screen

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-screen/professor-screen.module.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-screen/professor-screen.module.ts
@@ -1,6 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProfessorScreenComponent } from './professor-screen.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatTabsModule} from "@angular/material/tabs";
+// import {StudentLogoModule} from "../student-logo/student-logo.module";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+// import {StudentLessonScreenModule} from "../student-lesson-screen/student-lesson-screen.module";
+// import {StudentHistoryScreenModule} from "../student-history-screen/student-history-screen.module";
+// import {StudentNotificationScreenModule} from "../student-notification-screen/student-notification-screen.module";
+import { ProfessorLogoModule } from '../professor-logo/professor-logo.module';
+import { ProfessorLessonScreenModule } from '../professor-lesson-screen/professor-lesson-screen.module';
 
 
 
@@ -12,7 +22,13 @@ import { ProfessorScreenComponent } from './professor-screen.component';
     ProfessorScreenComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSidenavModule,
+    MatTabsModule,
+    ProfessorLogoModule,
+    MatIconModule,
+    MatButtonModule,
+    ProfessorLessonScreenModule
   ]
 })
 export class ProfessorScreenModule { }
