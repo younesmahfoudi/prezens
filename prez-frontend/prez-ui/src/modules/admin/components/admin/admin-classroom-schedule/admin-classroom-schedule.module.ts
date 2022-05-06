2ea27bf4e46662225206d6238e7e3c215d667ad3
younesmahfoudi <younesmfd@gmail.com>
2022-05-06 12:06:47 +0200

    amdin side students ok

---
+++ prez-frontend/prez-ui/src/modules/admin/components/admin/admin-classroom-schedule/admin-classroom-schedule.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {AdminClassroomScheduleComponent} from "./admin-classroom-schedule.component";
+import {LessonTimelineModule} from "../../../../lesson/components/lesson-timeline/lesson-timeline.module";
+import {MatIconModule} from "@angular/material/icon";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatButtonModule} from "@angular/material/button";
+
+
+
+@NgModule({
+  declarations: [
+      AdminClassroomScheduleComponent
+  ],
+    imports: [
+        CommonModule,
+        LessonTimelineModule,
+        MatIconModule,
+        MatDialogModule,
+        MatButtonModule
+    ]
+})
+export class AdminClassroomScheduleModule { }
---
+++ prez-frontend/prez-ui/src/modules/core/domain/user/user.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {User} from "./user.model";
+import {Observable} from "rxjs";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UserService {
+
+    constructor(private http: HttpClient) { }
+
+    public provideUser(user: User): Observable<User>{
+        return this.http.post<User>(`/api/users/`, user);
+    }
+}
