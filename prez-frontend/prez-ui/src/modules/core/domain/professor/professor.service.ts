9833f611bf6c7b7db443aee96208d80f70614502
TRUFFIER Thibaut <truffierth@eisti.eu>
2022-05-06 14:41:41 +0200

    professor screen

---
+++ prez-frontend/prez-ui/src/modules/core/domain/professor/professor.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Professor } from './professor.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfessorService {
+
+  private pathUrl: string = '/api/professors';
+
+  constructor(private http: HttpClient) { }
+
+  public getProfessor(professor_uid: number) : Observable<Professor>{
+    return this.http.get<Professor>(`${this.pathUrl}/${professor_uid}`)
+  }
+}
