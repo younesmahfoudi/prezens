c0a90cb00221df03f32a869d23e13895e6b95950
TRUFFIER Thibaut <truffierth@eisti.eu>
2022-05-06 14:41:41 +0200

    professor screen

--- prez-frontend/prez-ui/src/modules/core/domain/professor/professor.service.ts
+++ prez-frontend/prez-ui/src/modules/core/domain/professor/professor.service.ts
@@ -1,31 +1,26 @@
-import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {Professor, ProfessorCreate} from "./professor.model";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class ProfessorService {
 
-    private pathUrl: string = '/api/professors';
+  private pathUrl: string = '/api/professors';
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) { }
 
-    public getProfessor(professor_uid: number): Observable<Professor> {
-        return this.http.get<Professor>(`${this.pathUrl}/${professor_uid}`)
-    }
+  public getProfessor(professor_uid: number) : Observable<Professor>{
+    return this.http.get<Professor>(`${this.pathUrl}/${professor_uid}`)
+  }
 
-    public getProfessors(): Observable<Professor[]> {
+    public getProfessors(): Observable<Professor[]>{
         return this.http.get<Professor[]>(`/api/professors/`);
     }
 
-    public signupProfessor(professorCreate: ProfessorCreate): Observable<any> {
+    public signupProfessor(professorCreate: ProfessorCreate): Observable<any>{
         return this.http.post<any>(`/api/professors/signup`, professorCreate);
-    }
-
-    public deleteProfessor(professorUid: number): Observable<any>{
-        return this.http.delete<any>(`/api/professors/${professorUid}`);
     }
 }
