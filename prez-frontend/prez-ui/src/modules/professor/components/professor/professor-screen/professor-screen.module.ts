ea78ee054887c5b45af8a51fcce16e140695f3f0
TRUFFIER Thibaut <truffierth@eisti.eu>
2022-05-06 14:41:41 +0200

    professor screen

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-screen/professor-screen.module.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-screen/professor-screen.module.ts
@@ -1,4 +1,4 @@
-import {ProfessorScreenComponent} from './professor-screen.component';
+import { ProfessorScreenComponent } from './professor-screen.component';
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatSidenavModule} from "@angular/material/sidenav";
@@ -9,28 +9,26 @@ import {MatButtonModule} from "@angular/material/button";
 // import {StudentLessonScreenModule} from "../student-lesson-screen/student-lesson-screen.module";
 // import {StudentHistoryScreenModule} from "../student-history-screen/student-history-screen.module";
 // import {StudentNotificationScreenModule} from "../student-notification-screen/student-notification-screen.module";
-import {ProfessorLogoModule} from '../professor-logo/professor-logo.module';
-import {ProfessorLessonScreenModule} from '../professor-lesson-screen/professor-lesson-screen.module';
-import {ProfessorHistoryModule} from '../professor-history/professor-history.module';
+import { ProfessorLogoModule } from '../professor-logo/professor-logo.module';
+import { ProfessorLessonScreenModule } from '../professor-lesson-screen/professor-lesson-screen.module';
+
 
 
 @NgModule({
-    declarations: [
-        ProfessorScreenComponent
-    ],
-    exports: [
-        ProfessorScreenComponent
-    ],
-    imports: [
-        CommonModule,
-        MatSidenavModule,
-        MatTabsModule,
-        ProfessorLogoModule,
-        MatIconModule,
-        MatButtonModule,
-        ProfessorLessonScreenModule,
-        ProfessorHistoryModule
-    ]
+  declarations: [
+    ProfessorScreenComponent
+  ],
+  exports: [
+    ProfessorScreenComponent
+  ],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatTabsModule,
+    ProfessorLogoModule,
+    MatIconModule,
+    MatButtonModule,
+    ProfessorLessonScreenModule
+  ]
 })
-export class ProfessorScreenModule {
-}
+export class ProfessorScreenModule { }
