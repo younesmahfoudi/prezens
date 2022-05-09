350abf4332a9a951968e4f45653b8cb4232cfe92
TRUFFIER Thibaut <truffierth@eisti.eu>
2022-05-09 13:45:21 +0200

    professor history

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-history/professor-history.component.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-history/professor-history.component.ts
@@ -1,73 +1,63 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ClassroomService} from 'src/modules/core/domain/classroom/classroom.service';
-import {Lesson} from 'src/modules/core/domain/lesson/lesson.model';
-import {LessonService} from 'src/modules/core/domain/lesson/lesson.service';
-import {LessonElement} from 'src/modules/lesson/components/lesson-element/lesson-element.model';
-import {LessonElementService} from 'src/modules/lesson/components/lesson-element/lesson-element.service';
-import {ProfessorElement} from '../professor-element/professor-element.model';
-import {MatDialog} from '@angular/material/dialog';
-import {ProfessorDialogComponent} from "../professor-dialog/professor-dialog.component";
-import {RegisterService} from "../../../../core/domain/register/register.service";
-import {RegisterElement} from "../../../../register/components/register-element/register-element.model";
+import { Component, Input, OnInit } from '@angular/core';
+import { throwError } from 'rxjs';
+import { ClassroomElement } from 'src/modules/classroom/components/classroom-element/classroom-element.model';
+import { Classroom } from 'src/modules/core/domain/classroom/classroom.model';
+import { ClassroomService } from 'src/modules/core/domain/classroom/classroom.service';
+import { Lesson } from 'src/modules/core/domain/lesson/lesson.model';
+import { LessonService } from 'src/modules/core/domain/lesson/lesson.service';
+import { LessonElement } from 'src/modules/lesson/components/lesson-element/lesson-element.model';
+import { LessonElementService } from 'src/modules/lesson/components/lesson-element/lesson-element.service';
+import { ProfessorElement } from '../professor-element/professor-element.model';
 
 @Component({
-    selector: 'prez-professor-history',
-    templateUrl: './professor-history.component.html',
-    styleUrls: ['./professor-history.component.scss']
+  selector: 'prez-professor-history',
+  templateUrl: './professor-history.component.html',
+  styleUrls: ['./professor-history.component.scss']
 })
 export class ProfessorHistoryComponent implements OnInit {
 
-    constructor(
-        private lessonService: LessonService,
-        private lessonElementService: LessonElementService,
-        private classroomService: ClassroomService,
-        private registerService: RegisterService,
-        private dialog: MatDialog
-    ) {
-    }
+  constructor(
+    private lessonService: LessonService,
+    private lessonElementService: LessonElementService,
+    private classroomService: ClassroomService
+  ) { }
 
 
-    @Input() professorElement?: ProfessorElement;
-    // @Input() classroomElement?: ClassroomElement;
-    public lessonElements?: LessonElement[];
-    private lessonData?: Lesson[];
-    private registerElement?: RegisterElement;
+  @Input() professorElement?: ProfessorElement;
+  // @Input() classroomElement?: ClassroomElement;
+  public lessonElements?: LessonElement[];
+  private lessonData?: Lesson[];
+  
+  
+  ngOnInit(): void {
+    debugger
+    this.initData();
+  }
 
 
-    ngOnInit(): void {
-        this.initData();
-    }
+  private initData(): void{
+    this.initLessonData(this.professorElement?.uid);
+  }
 
+  private initLessonData(professorUid?: number): void{
+    if (!professorUid) return;
+    this.lessonService.getLessonByProfessor(professorUid).subscribe(
+      lessons => {
+        this.lessonData = lessons;
+        this.lessonElements = this.lessonElementService.mapLessonElements(this.lessonData);
+      },
+      error => {
+        console.warn(error);
+      }
+    )
+  }
 
-    private initData(): void {
-        this.initLessonData(this.professorElement?.uid);
-    }
+  private getClassroomByUid(classroomUid: number): void{
+    this.classroomService.getClassroom(classroomUid).subscribe(
+      classroom => {
+        return classroom.promotion
+      }
+    )
 
-    private initLessonData(professorUid?: number): void {
-        if (!professorUid) return;
-        this.lessonService.getLessonsByDate(professorUid,"before").subscribe(
-            lessons => {
-                this.lessonData = lessons;
-                this.lessonElements = this.lessonElementService.mapLessonElements(this.lessonData);
-            },
-            error => {
-                console.warn(error);
-            }
-        )
-    }
-
-    openDialog(lesson: LessonElement): void {
-        const dialogRef = this.dialog.open(ProfessorDialogComponent, {
-            data: {
-                lessonElement: lesson
-            }
-        });
-
-        dialogRef.afterClosed().subscribe(result => {
-            console.log('Redirect vers register screen');
-            this.initData()
-        });
-    }
-
-
+  }
 }
--- prez-frontend/prez-ui/src/modules/professor/professor.module.ts
+++ prez-frontend/prez-ui/src/modules/professor/professor.module.ts
@@ -1,22 +1,16 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {ProfessorComponent} from './professor.component';
-import {ProfessorScreenModule} from './components/professor/professor-screen/professor-screen.module';
-import { ProfessorRegisterScreenComponent } from './components/professor/professor-register-screen/professor-register-screen.component';
-import { ProfessorRegisterSignComponent } from './components/professor/professor-register-sign/professor-register-sign.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProfessorComponent } from './professor.component';
+import { ProfessorScreenModule } from './components/professor/professor-screen/professor-screen.module';
 
 
 @NgModule({
-    declarations: [
-        ProfessorComponent,
-    ],
-    exports: [
-        ProfessorComponent
-    ],
-    imports: [
-        CommonModule,
-        ProfessorScreenModule
-    ]
+  declarations: [
+    ProfessorComponent
+  ],
+  imports: [
+    CommonModule,
+    ProfessorScreenModule
+  ]
 })
-export class ProfessorModule {
-}
+export class ProfessorModule { }
