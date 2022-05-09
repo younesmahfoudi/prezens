7b5aa838af3cf995ee50f9376441a42bacc4215c
TRUFFIER Thibaut <truffierth@eisti.eu>
2022-05-09 13:45:21 +0200

    professor history

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-lesson-screen/professor-lesson-screen.component.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-lesson-screen/professor-lesson-screen.component.ts
@@ -1,64 +1,52 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
-import {ClassroomElement} from 'src/modules/classroom/components/classroom-element/classroom-element.model';
-import {Classroom} from 'src/modules/core/domain/classroom/classroom.model';
-import {Lesson} from 'src/modules/core/domain/lesson/lesson.model';
-import {LessonService} from 'src/modules/core/domain/lesson/lesson.service';
-import {LessonElement} from 'src/modules/lesson/components/lesson-element/lesson-element.model';
-import {LessonElementService} from 'src/modules/lesson/components/lesson-element/lesson-element.service';
-import {ProfessorElement} from '../professor-element/professor-element.model';
-import {LessonFilter} from "../../../../admin/components/admin/lesson/admin-lesson-toolbar/lesson-filter.model";
+import { Component, Input, OnInit } from '@angular/core';
+import { ClassroomElement } from 'src/modules/classroom/components/classroom-element/classroom-element.model';
+import { Classroom } from 'src/modules/core/domain/classroom/classroom.model';
+import { Lesson } from 'src/modules/core/domain/lesson/lesson.model';
+import { LessonService } from 'src/modules/core/domain/lesson/lesson.service';
+import { LessonElement } from 'src/modules/lesson/components/lesson-element/lesson-element.model';
+import { LessonElementService } from 'src/modules/lesson/components/lesson-element/lesson-element.service';
+import { ProfessorElement } from '../professor-element/professor-element.model';
 
 @Component({
-    selector: 'prez-professor-lesson-screen',
-    templateUrl: './professor-lesson-screen.component.html',
-    styleUrls: ['./professor-lesson-screen.component.scss']
+  selector: 'prez-professor-lesson-screen',
+  templateUrl: './professor-lesson-screen.component.html',
+  styleUrls: ['./professor-lesson-screen.component.scss']
 })
-export class ProfessorLessonScreenComponent implements OnInit,OnChanges {
+export class ProfessorLessonScreenComponent implements OnInit {
 
-    constructor(
-        private lessonService: LessonService,
-        private lessonElementService: LessonElementService
-    ) {
-    }
-
-
-    @Input() professorElement?: ProfessorElement;
-    @Input() classroomElement?: ClassroomElement;
-    @Input() lessonFilter: LessonFilter;
-    public lessonElements?: LessonElement[];
-    private lessonData?: Lesson[];
-    private classroomData?: Classroom;
-
-    ngOnInit(): void {
-        this.initData();
-    }
-
-    ngOnChanges(changes: SimpleChanges) {
-        if (changes["lessonFilter"] && !changes["lessonFilter"].isFirstChange()){
-            this.initData();
-        }
-    }
+  constructor(
+    private lessonService: LessonService,
+    private lessonElementService: LessonElementService
+  ) { }
 
 
-    private initData(): void {
-        this.initLessonData(this.lessonFilter);
+  @Input() professorElement?: ProfessorElement;
+  @Input() classroomElement?: ClassroomElement;
+  public lessonElements?: LessonElement[];
+  private lessonData?: Lesson[];
+  private classroomData?: Classroom;
 
-    }
+  ngOnInit(): void {
+    this.initData();
+  }
 
-    private initLessonData(lessonFilter: LessonFilter): void {
-        if (!lessonFilter) return;
-        this.lessonService.getLessonFiltered(lessonFilter).subscribe(
-            lessons => {
-                this.lessonData = lessons;
-                this.lessonElements = this.lessonElementService.mapLessonElements(this.lessonData);
-            },
-            error => {
-                console.warn(error);
-            }
-        )
-    }
 
-    public applyFilter(lessonFilter: LessonFilter): void {
-        this.initLessonData(lessonFilter);
-    }
+  private initData(): void{
+    this.initLessonData(this.professorElement?.uid);
+    
+  }
+
+  private initLessonData(professorUid?: number): void{
+
+    if (!professorUid) return;
+    this.lessonService.getLessonsByProfessor(professorUid).subscribe(
+      lessons => {
+        this.lessonData = lessons;
+        this.lessonElements = this.lessonElementService.mapLessonElements(this.lessonData);
+      },
+      error => {
+        console.warn(error);
+      }
+    )
+  }
 }
