55d54cc3501d49935b6d57d526c80bfc02e806cb
Thibaut TRUFFIER <truffierth@cy-tech.fr>
2022-05-10 15:31:17 +0200

    Professor toolbar

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-history/professor-history.component.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-history/professor-history.component.ts
@@ -1,55 +1,53 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { throwError } from 'rxjs';
-import { ClassroomElement } from 'src/modules/classroom/components/classroom-element/classroom-element.model';
-import { Classroom } from 'src/modules/core/domain/classroom/classroom.model';
-import { ClassroomService } from 'src/modules/core/domain/classroom/classroom.service';
-import { Lesson } from 'src/modules/core/domain/lesson/lesson.model';
-import { LessonService } from 'src/modules/core/domain/lesson/lesson.service';
-import { LessonElement } from 'src/modules/lesson/components/lesson-element/lesson-element.model';
-import { LessonElementService } from 'src/modules/lesson/components/lesson-element/lesson-element.service';
-import { ProfessorElement } from '../professor-element/professor-element.model';
+import {Component, Input, OnInit} from '@angular/core';
+import {ClassroomService} from 'src/modules/core/domain/classroom/classroom.service';
+import {Lesson} from 'src/modules/core/domain/lesson/lesson.model';
+import {LessonService} from 'src/modules/core/domain/lesson/lesson.service';
+import {LessonElement} from 'src/modules/lesson/components/lesson-element/lesson-element.model';
+import {LessonElementService} from 'src/modules/lesson/components/lesson-element/lesson-element.service';
+import {ProfessorElement} from '../professor-element/professor-element.model';
 
 @Component({
-  selector: 'prez-professor-history',
-  templateUrl: './professor-history.component.html',
-  styleUrls: ['./professor-history.component.scss']
+    selector: 'prez-professor-history',
+    templateUrl: './professor-history.component.html',
+    styleUrls: ['./professor-history.component.scss']
 })
 export class ProfessorHistoryComponent implements OnInit {
 
-  constructor(
-    private lessonService: LessonService,
-    private lessonElementService: LessonElementService,
-    private classroomService: ClassroomService
-  ) { }
+    constructor(
+        private lessonService: LessonService,
+        private lessonElementService: LessonElementService,
+        private classroomService: ClassroomService
+    ) {
+    }
 
 
-  @Input() professorElement?: ProfessorElement;
-  // @Input() classroomElement?: ClassroomElement;
-  public lessonElements?: LessonElement[];
-  private lessonData?: Lesson[];
+    @Input() professorElement?: ProfessorElement;
+    // @Input() classroomElement?: ClassroomElement;
+    public lessonElements?: LessonElement[];
+    private lessonData?: Lesson[];
 
 
-  ngOnInit(): void {
-    this.initData();
-  }
+    ngOnInit(): void {
+        this.initData();
+    }
 
 
-  private initData(): void{
-    this.initLessonData(this.professorElement?.uid);
-  }
+    private initData(): void {
+        this.initLessonData(this.professorElement?.uid);
+    }
 
-  private initLessonData(professorUid?: number): void{
-    if (!professorUid) return;
-    this.lessonService.getLessonsByProfessor(professorUid).subscribe(
-      lessons => {
-        this.lessonData = lessons;
-        this.lessonElements = this.lessonElementService.mapLessonElements(this.lessonData);
-      },
-      error => {
-        console.warn(error);
-      }
-    )
-  }
+    private initLessonData(professorUid?: number): void {
+        if (!professorUid) return;
+        this.lessonService.getLessonsByProfessor(professorUid).subscribe(
+            lessons => {
+                this.lessonData = lessons;
+                this.lessonElements = this.lessonElementService.mapLessonElements(this.lessonData);
+            },
+            error => {
+                console.warn(error);
+            }
+        )
+    }
 
   private getClassroomByUid(classroomUid: number): void{
     this.classroomService.getClassroom(classroomUid).subscribe(
