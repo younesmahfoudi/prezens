fac1517e64428cb9df339ed893213abc22e14b6d
Thibaut TRUFFIER <truffierth@cy-tech.fr>
2022-05-10 15:31:17 +0200

    Professor toolbar

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-history/professor-history.component.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-history/professor-history.component.ts
@@ -1,19 +1,18 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {ClassroomService} from 'src/modules/core/domain/classroom/classroom.service';
-import {Lesson} from 'src/modules/core/domain/lesson/lesson.model';
-import {LessonService} from 'src/modules/core/domain/lesson/lesson.service';
-import {LessonElement} from 'src/modules/lesson/components/lesson-element/lesson-element.model';
-import {LessonElementService} from 'src/modules/lesson/components/lesson-element/lesson-element.service';
-import {ProfessorElement} from '../professor-element/professor-element.model';
-import {MatDialog} from '@angular/material/dialog';
-import {ProfessorDialogComponent} from "../professor-dialog/professor-dialog.component";
-import {RegisterService} from "../../../../core/domain/register/register.service";
-import {RegisterElement} from "../../../../register/components/register-element/register-element.model";
+import { Component, Input, OnInit } from '@angular/core';
+import { throwError } from 'rxjs';
+import { ClassroomElement } from 'src/modules/classroom/components/classroom-element/classroom-element.model';
+import { Classroom } from 'src/modules/core/domain/classroom/classroom.model';
+import { ClassroomService } from 'src/modules/core/domain/classroom/classroom.service';
+import { Lesson } from 'src/modules/core/domain/lesson/lesson.model';
+import { LessonService } from 'src/modules/core/domain/lesson/lesson.service';
+import { LessonElement } from 'src/modules/lesson/components/lesson-element/lesson-element.model';
+import { LessonElementService } from 'src/modules/lesson/components/lesson-element/lesson-element.service';
+import { ProfessorElement } from '../professor-element/professor-element.model';
 
 @Component({
-    selector: 'prez-professor-history',
-    templateUrl: './professor-history.component.html',
-    styleUrls: ['./professor-history.component.scss']
+  selector: 'prez-professor-history',
+  templateUrl: './professor-history.component.html',
+  styleUrls: ['./professor-history.component.scss']
 })
 export class ProfessorHistoryComponent implements OnInit {
 
@@ -31,14 +30,14 @@ export class ProfessorHistoryComponent implements OnInit {
     private lessonData?: Lesson[];
     private registerElement?: RegisterElement;
 
-    ngOnInit(): void {
-        this.initData();
-    }
-
-
-  private initData(): void{
-    this.initLessonData(this.professorElement?.uid);
+  ngOnInit(): void {
+    this.initData();
   }
+
+
+    private initData(): void {
+        this.initLessonData(this.professorElement?.uid);
+    }
 
     private initLessonData(professorUid?: number): void {
         if (!professorUid) return;
