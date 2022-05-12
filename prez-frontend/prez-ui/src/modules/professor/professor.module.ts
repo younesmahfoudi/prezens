02b47fcd103fc15dd69169fe76034979f5af88a2
Thibaut TRUFFIER <truffierth@cy-tech.fr>
2022-05-12 17:43:35 +0200

    Professor Register screen

--- prez-frontend/prez-ui/src/modules/professor/professor.module.ts
+++ prez-frontend/prez-ui/src/modules/professor/professor.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfessorComponent } from './professor.component';
 import { ProfessorScreenModule } from './components/professor/professor-screen/professor-screen.module';
-import { ProfessorLessonScreenComponent } from './components/professor/professor-lesson-screen/professor-lesson-screen.component';
-import { ProfessorHistoryComponent } from './components/professor/professor-history/professor-history.component';
 
 
 @NgModule({
   declarations: [
-    ProfessorComponent,
+    ProfessorComponent
   ],
   imports: [
     CommonModule,
