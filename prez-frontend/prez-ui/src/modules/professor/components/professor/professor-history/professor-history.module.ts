b6ee02c0fcadddcb123cd6b30a16272e2a285c6a
Thibaut TRUFFIER <truffierth@cy-tech.fr>
2022-05-12 17:43:35 +0200

    Professor Register screen

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-history/professor-history.module.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-history/professor-history.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProfessorHistoryComponent } from './professor-history.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ProfessorHistoryComponent} from './professor-history.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {ProfessorDialogModule} from "../professor-dialog/professor-dialog.module";
 
 
 @NgModule({
-  declarations: [
-    ProfessorHistoryComponent
-  ],
-  exports: [
-    ProfessorHistoryComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        ProfessorHistoryComponent
+    ],
+    exports: [
+        ProfessorHistoryComponent
+    ],
+    imports: [
+        CommonModule,
+        MatCardModule,
+        MatIconModule,
+        MatButtonModule,
+        ProfessorDialogModule
+    ]
 })
-export class ProfessorHistoryModule { }
+export class ProfessorHistoryModule {
+}
