eb96e0f4edcbcbacd1b5f95fd85654f41e5634e9
younesmahfoudi <younesmfd@gmail.com>
2022-05-17 00:22:18 +0200

    404 not found and css

--- prez-frontend/prez-ui/src/modules/common/not-found/not-found.module.ts
+++ prez-frontend/prez-ui/src/modules/common/not-found/not-found.module.ts
@@ -1,6 +1,7 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NotFoundComponent} from "./not-found.component";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
@@ -8,7 +9,8 @@ import {NotFoundComponent} from "./not-found.component";
         NotFoundComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        MatButtonModule
     ]
 })
 export class NotFoundModule { }
