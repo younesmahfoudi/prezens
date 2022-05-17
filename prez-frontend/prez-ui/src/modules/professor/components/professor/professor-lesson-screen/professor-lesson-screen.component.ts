51ccda6999f853b2af8e6704dfb7335ff7e7f3da
Thibaut TRUFFIER <truffierth@cy-tech.fr>
2022-05-17 16:29:58 +0200

    professor lesson timeline Fini

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-lesson-screen/professor-lesson-screen.component.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-lesson-screen/professor-lesson-screen.component.ts
@@ -7,34 +7,48 @@ import {LessonElement} from 'src/modules/lesson/components/lesson-element/lesson
 import {LessonElementService} from 'src/modules/lesson/components/lesson-element/lesson-element.service';
 import {ProfessorElement} from '../professor-element/professor-element.model';
 import {LessonFilter} from "../../../../admin/components/admin/lesson/admin-lesson-toolbar/lesson-filter.model";
+import {ClassroomService} from "../../../../core/domain/classroom/classroom.service";
+import {ClassroomElementService} from "../../../../classroom/components/classroom-element/classroom-element.service";
+import {
+    AdminLessonDetailComponent
+} from "../../../../admin/components/admin/lesson/admin-lesson-detail/admin-lesson-detail.component";
+import {MatDialog} from "@angular/material/dialog";
+import {ProfessorDialogComponent} from "../professor-dialog/professor-dialog.component";
+import {ProfessorRegisterDialogComponent} from "../professor-register-dialog/professor-register-dialog.component";
 
 @Component({
     selector: 'prez-professor-lesson-screen',
     templateUrl: './professor-lesson-screen.component.html',
     styleUrls: ['./professor-lesson-screen.component.scss']
 })
-export class ProfessorLessonScreenComponent implements OnInit,OnChanges {
+export class ProfessorLessonScreenComponent implements OnInit, OnChanges {
+
 
     constructor(
         private lessonService: LessonService,
-        private lessonElementService: LessonElementService
+        private lessonElementService: LessonElementService,
+        private classroomService: ClassroomService,
+        private classroomElementService: ClassroomElementService,
+        public dialog: MatDialog
     ) {
     }
 
-
-    @Input() professorElement?: ProfessorElement;
-    @Input() classroomElement?: ClassroomElement;
+    private currentDate: Date;
+    private professorElement?: ProfessorElement;
+    private classroomElement?: ClassroomElement;
     @Input() lessonFilter: LessonFilter;
     public lessonElements?: LessonElement[];
     private lessonData?: Lesson[];
     private classroomData?: Classroom;
+    private singleLessonData?: Lesson;
+    private lessonElement?: LessonElement
 
     ngOnInit(): void {
         this.initData();
     }
 
     ngOnChanges(changes: SimpleChanges) {
-        if (changes["lessonFilter"] && !changes["lessonFilter"].isFirstChange()){
+        if (changes["lessonFilter"] && !changes["lessonFilter"].isFirstChange()) {
             this.initData();
         }
     }
@@ -42,7 +56,7 @@ export class ProfessorLessonScreenComponent implements OnInit,OnChanges {
 
     private initData(): void {
         this.initLessonData(this.lessonFilter);
-
+        this.currentDate = new Date()
     }
 
     private initLessonData(lessonFilter: LessonFilter): void {
@@ -60,5 +74,54 @@ export class ProfessorLessonScreenComponent implements OnInit,OnChanges {
 
     public applyFilter(lessonFilter: LessonFilter): void {
         this.initLessonData(lessonFilter);
+    }
+
+    public openLessonDetailDialog(lessonScheduleData: any) {
+        const lessonUid: number = lessonScheduleData.data.uid;
+        console.log(lessonUid)
+        this.lessonService.getLesson(lessonUid).subscribe(
+            lesson => {
+                this.singleLessonData = lesson;
+                this.lessonElement = this.lessonElementService.mapLessonElement(this.singleLessonData);
+                this.openDialog(this.lessonElement)
+            },
+            error => {
+                console.warn(error);
+            }
+        )
+    }
+
+
+    openDialog(lessonElement: LessonElement) {
+        debugger
+        if (!lessonElement) return;
+        if (this.compareDate((lessonElement.end_at))) {
+            const dialogRef = this.dialog.open(
+                ProfessorDialogComponent, {
+                    data: {
+                        lessonElement: lessonElement
+                    }
+                });
+            dialogRef.afterClosed().subscribe(result => {
+                this.initLessonData(this.lessonFilter);
+                console.log(`Dialog result: ${result}`);
+            });
+        }
+        else {
+            const dialogRef = this.dialog.open(
+                ProfessorRegisterDialogComponent, {
+                    data: {
+                        lessonElement: lessonElement
+                    }
+                });
+            dialogRef.afterClosed().subscribe(result => {
+                this.initLessonData(this.lessonFilter);
+                console.log(`Dialog result: ${result}`);
+            });
+        }
+    }
+
+    public compareDate(lessonDate: Date): boolean{
+        return (this.currentDate > new Date(lessonDate))
     }
 }
