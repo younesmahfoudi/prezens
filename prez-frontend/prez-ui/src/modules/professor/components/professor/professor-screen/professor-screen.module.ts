6b0934bf5a1a19223ca2fa4fb6cd35b1a7e2f6e9
Thibaut TRUFFIER <truffierth@cy-tech.fr>
2022-05-17 16:40:53 +0200

    professor lesson timeline Fini

--- prez-frontend/prez-ui/src/modules/professor/components/professor/professor-screen/professor-screen.module.ts
+++ prez-frontend/prez-ui/src/modules/professor/components/professor/professor-screen/professor-screen.module.ts
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import {ProfessorScreenComponent} from './professor-screen.component';
+=======
+import { ProfessorScreenComponent } from './professor-screen.component';
+>>>>>>> 82f7875 (professor screen)
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatSidenavModule} from "@angular/material/sidenav";
@@ -9,6 +13,7 @@ import {MatButtonModule} from "@angular/material/button";
 // import {StudentLessonScreenModule} from "../student-lesson-screen/student-lesson-screen.module";
 // import {StudentHistoryScreenModule} from "../student-history-screen/student-history-screen.module";
 // import {StudentNotificationScreenModule} from "../student-notification-screen/student-notification-screen.module";
+<<<<<<< HEAD
 import {ProfessorLogoModule} from '../professor-logo/professor-logo.module';
 import {ProfessorLessonScreenModule} from '../professor-lesson-screen/professor-lesson-screen.module';
 import {ProfessorHistoryModule} from '../professor-history/professor-history.module';
@@ -33,6 +38,29 @@ import {ProfessorRegisterScreenModule} from "../professor-register-screen/profes
         ProfessorHistoryModule,
         ProfessorRegisterScreenModule
     ]
+=======
+import { ProfessorLogoModule } from '../professor-logo/professor-logo.module';
+import { ProfessorLessonScreenModule } from '../professor-lesson-screen/professor-lesson-screen.module';
+
+
+
+@NgModule({
+  declarations: [
+    ProfessorScreenComponent
+  ],
+  exports: [
+    ProfessorScreenComponent
+  ],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatTabsModule,
+    ProfessorLogoModule,
+    MatIconModule,
+    MatButtonModule,
+    ProfessorLessonScreenModule
+  ]
+>>>>>>> 82f7875 (professor screen)
 })
 export class ProfessorScreenModule {
 }
