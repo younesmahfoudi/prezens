e457550bd9e495b9ab5ebb26ee7feca53d8c21b6
younesmahfoudi <younesmfd@gmail.com>
2022-05-18 23:28:37 +0200

    Current date in Docker issue fixed

--- prez-backend/prez-app/database.py
+++ prez-backend/prez-app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root1234@localhost:6033/prezens_db"
+# SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root1234@localhost:6033/prezens_db"
 SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root1234@db/prezens_db"
 # SQLALCHEMY_DATABASE_URL = "mysql://test:test@localhost:6033/prezens_db"
 
